bfa265836e2c311efdc9b7ed8868bcf328133ec3
Calvin Haviandy <91955694+calvinhaviandy@users.noreply.github.com>
2022-12-18 18:56:16 +0700

    Add files via upload

---
+++ Perulangan2.0/src/perulangan2/pkg0/Perulangan20.java
@@ -0,0 +1,28 @@
+
+package perulangan2.pkg0;
+
+
+public class Perulangan20 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("=============================");
+        System.out.println("Program Perulangan");
+        System.out.println("=============================");
+        
+        
+        
+        int bilangan = 0;
+        
+        while(bilangan <=100){
+            if(bilangan % 2 ==0){
+                System.out.println(" Bilangan Genap Ke- " + bilangan);
+            }
+            
+            bilangan++;
+        }
+        
+    }
+    
+}
